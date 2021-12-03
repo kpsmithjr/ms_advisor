4712efe301ff6e804a8a2a855b3ce4d98194e4e3
rn5w9 <rn5w9@umsystem.edu>
2021-12-02 23:07:12 -0600

    -added course descriptions, and shows course descriptions in planner on hover

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { Draggable} from "react-beautiful-dnd";
 
 import Course from "../types/courseType";
+import cs_courses from "../data/cs_courses.json"
 
 interface StyledDivProps {
 	isDragging: boolean;
@@ -21,6 +22,39 @@ interface ICourseCard {
 	index: number;
 }
 
+const getCourseIdx = (dept: string, num: number) => {
+	for (let i = 0; i < cs_courses.length; ++i) {
+		if ((cs_courses[i].dept === dept) && (cs_courses[i].num === num)) {
+			return i;
+		}
+	}
+	return -1;
+};
+
+//The description is based on the course container id's being set to dept,num csv string in CourseCard below
+//TODO: is it better to show the description on mouseenter? or onclick, followed by hiding ondrag + onmouseup?
+const showDescription = (event: any) => {
+	const dept = event.target.id.split(",")[0];
+	const num = parseInt(event.target.id.split(",")[1]);
+	var courseIdx = getCourseIdx(dept, num);
+	//console.log(event.target);
+	//console.dir(event.target);
+	if (courseIdx >= 0) {
+		var newStr = event.target.textContent += "\n\n" + cs_courses[courseIdx].description;
+		event.target.textContent = newStr;
+		event.target.innerHTML = newStr.replace(/\r/g, '').replace(/\n/g, '<br>');
+    }
+}
+
+const hideDescription = (event: any) => {
+	const dept = event.target.id.split(",")[0];
+	const num = parseInt(event.target.id.split(",")[1]);
+	var courseIdx = getCourseIdx(dept, num);
+	if (courseIdx >= 0) {
+		event.target.textContent = cs_courses[courseIdx].dept + " " + cs_courses[courseIdx].num + " - " + cs_courses[courseIdx].name;
+    }
+}
+
 const CourseCard = ({course, index}: ICourseCard) => {
 	return (
 		<Draggable
@@ -32,11 +66,15 @@ const CourseCard = ({course, index}: ICourseCard) => {
 				<Container
 					{...provided.draggableProps}
 					{...provided.dragHandleProps}
+					id={course.dept + "," + course.num}
 					ref={provided.innerRef}
 					isDragging={snapshot.isDragging}
 					isAvailable={false}
+					onMouseEnter={showDescription}
+					onMouseLeave={hideDescription}
+					onMouseDown={hideDescription}
 				>
-					{course.id  + " - " + course.name}
+					{course.id + " - " + course.name}
 				</Container>
 			)}			
 		</Draggable>		
