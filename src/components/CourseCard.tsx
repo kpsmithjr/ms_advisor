f7a4896d28e1ce88a50aab47a60f6131423acb17
rn5w9 <rn5w9@umsystem.edu>
2021-12-14 20:38:40 -0600

    -adds info on when courses are offered to descriptions in planning window

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -5,7 +5,11 @@ import Course from "../types/courseType";
 import cs_courses from "../data/cs_courses.json"
 import math_courses from "../data/math_courses.json";
 
+import cs_rotation from "../data/cs_rotation.json";
+import math_rotation from "../data/math_rotation.json";
+
 const allTheCourses = cs_courses.concat(math_courses);
+const allTheRotations = cs_rotation.concat(math_rotation);
 
 interface StyledDivProps {
 	isDragging: boolean;
@@ -36,6 +40,46 @@ const getCourseIdx = (dept: string, num: number) => {
 	return -1;
 };
 
+const getRotationIdx = (dept: string, num: number) => {
+	for (let i = 0; i < allTheRotations.length; ++i) {
+		if ((allTheRotations[i].dept === dept) && (allTheRotations[i].num === num)) {
+			return i;
+		}
+	}
+	return -1;
+};
+
+const getAvailabilityStr = (dept: string, num: number) => {
+	var availStr: string = "";
+	const rotId = getRotationIdx(dept, num);
+	if (rotId >= 0) {
+		const rotInfo = allTheRotations[getRotationIdx(dept, num)];
+
+		if (rotInfo) {
+			const sems = [];
+			if (rotInfo.springSem) { sems.push("Spring") };
+			if (rotInfo.summerSem) { sems.push("Summer") };
+			if (rotInfo.fallSem) { sems.push("Fall") };
+			availStr += "Semesters Offered: " + sems.join(", ") + "\n";
+
+			if (!(rotInfo.evenYr && rotInfo.oddYr)) {
+				const yrs = [];
+				if (rotInfo.evenYr) { yrs.push("Even") };
+				if (rotInfo.oddYr) { yrs.push("Odd") };
+				availStr += "Years Offered: " + yrs.join(", ") + "\n";
+			}
+
+			const times = [];
+			if (rotInfo.day) { times.push("Day") };
+			if (rotInfo.evening) { times.push("Evening") };
+			if (rotInfo.online) { times.push("Online") };
+			if (rotInfo.arranged) { times.push("Arranged") };
+			availStr += "Times Offered: " + times.join(", ");
+        }
+	}
+	return availStr;
+}
+
 //The description is based on the course container id's being set to dept,num csv string in CourseCard below
 //TODO: is it better to show the description on mouseenter? or onclick, followed by hiding ondrag + onmouseup?
 const showDescription = (event: any) => {
@@ -45,9 +89,15 @@ const showDescription = (event: any) => {
 	//console.log(event.target);
 	//console.dir(event.target);
 	if (courseIdx >= 0) {
-		var newStr = event.target.textContent += "\n\n" + allTheCourses[courseIdx].description;
-		event.target.textContent = newStr;
-		event.target.innerHTML = newStr.replace(/\r/g, '').replace(/\n/g, '<br>');
+		const course = allTheCourses[courseIdx];
+
+		if (course) {
+			var newStr = event.target.textContent += "\n\n" + getAvailabilityStr(course.dept, course.num) + "\n\n" + course.description;
+			event.target.textContent = newStr;
+			event.target.innerHTML = newStr.replace(/\r/g, '').replace(/\n/g, '<br>');
+        }
+
+		
     }
 }
 
